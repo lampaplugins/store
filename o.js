d53d4c1070ce2825aaf7096f7ab43213d50bf1f5
lampaplugins <152893536+lampaplugins@users.noreply.github.com>
2024-06-20 11:35:25 +0500

    Update o.js

--- o.js
+++ o.js
@@ -12,7 +12,8 @@
                 size: "large",
                 mask: !0,
                 onBack: function() {
-                    Lampa.Modal.close(), Lampa.Controller.toggle(enabled)
+                    Lampa.Modal.close(), Lampa.Controller.toggle(enabled);
+		$(".modal--large").remove();
                 },
                 onSelect: function() {}
             });
@@ -26,6 +27,7 @@
 	function startPlugin() {
 		window.otzyv_plugin = true;
 		Lampa.Listener.follow('full', function (e) {
+			$(".button--otzyv").remove();
 			if (e.type == 'complite') {
 					var num=0;
 					$('.full-start-new__buttons').append('<div class="full-start__button selector button--otzyv"><svg height="34" viewBox="0 0 28 34" fill="none" xmlns="http://www.w3.org/2000/svg"> <rect x="1.5" y="1.5" width="25" height="31" rx="2.5" stroke="currentColor" stroke-width="3"></rect><rect x="6" y="7" width="9" height="9" rx="1" fill="currentColor"></rect><rect x="6" y="19" width="16" height="3" rx="1.5" fill="currentColor"></rect><rect x="6" y="25" width="11" height="3" rx="1.5" fill="currentColor"></rect><rect x="17" y="7" width="5" height="3" rx="1.5" fill="currentColor"></rect> </svg><span>Отзывы</span></div>');
