166247251d6575b5e37960fa7c7f35961db63cc5
lampaplugins <152893536+lampaplugins@users.noreply.github.com>
2024-07-23 11:37:47 +0500

    Update o.js

--- o.js
+++ o.js
@@ -2,28 +2,25 @@
 	'use strict';
 	var num;
 	function otzyv_kp_imdb(kpid,imdbid,num) {
-			
 			$.get('https://api.skaz.tv/otzyv.php?kp='+kpid+'&tmdb='+imdbid+'&num='+num, function (data) {
 			var modal = $('<div><div class="broadcast__text" style="text-align:left"><div class="otzyv">'+data+'</div></div></div>');
 			var enabled = Lampa.Controller.enabled().name;
 			Lampa.Modal.open({
-                title: "",
-                html: modal,
-                size: "large",
-                mask: !0,
-                onBack: function() {
-                    Lampa.Modal.close(), Lampa.Controller.toggle(enabled);
-		$(".modal--large").remove();
-                },
-                onSelect: function() {}
-            });
+             		title: "",
+                	html: modal,
+                	size: "large",
+                	mask: !0,
+                	onBack: function() {
+                    		Lampa.Modal.close(), Lampa.Controller.toggle(enabled);
+				$(".modal--large").remove();
+                		},
+                	onSelect: function() {}
+            		});
 			});
 			
 	}
 	$('.otzyvb').on('hover:enter', function () {
-		console.log(123);
 	});
-	
 	function startPlugin() {
 		window.otzyv_plugin = true;
 		Lampa.Listener.follow('full', function (e) {
@@ -31,7 +28,7 @@
 			if (e.type == 'complite') {
 					var num=0;
 					$('.full-start-new__buttons').append('<div class="full-start__button selector button--otzyv"><svg height="34" viewBox="0 0 28 34" fill="none" xmlns="http://www.w3.org/2000/svg"> <rect x="1.5" y="1.5" width="25" height="31" rx="2.5" stroke="currentColor" stroke-width="3"></rect><rect x="6" y="7" width="9" height="9" rx="1" fill="currentColor"></rect><rect x="6" y="19" width="16" height="3" rx="1.5" fill="currentColor"></rect><rect x="6" y="25" width="11" height="3" rx="1.5" fill="currentColor"></rect><rect x="17" y="7" width="5" height="3" rx="1.5" fill="currentColor"></rect> </svg><span>Отзывы</span></div>');
-					$.get('https://api.skaz.tv/ts.php?kp='+e.data.movie['kinopoisk_id']+'&imdb='+e.data.movie['imdb_id'], function (data) {
+					if (Lampa.Account.logged()) $.get('https://api.skaz.tv/ts.php?kp='+e.data.movie['kinopoisk_id']+'&imdb='+e.data.movie['imdb_id'], function (data) {
 					if (data!='') $('.full-start-new__details').append('<span class="full-start-new__split">●</span><span>Качество: '+data+'</span>');
 					});
 					$('.button--otzyv').on('hover:enter', function (card) {
@@ -44,6 +41,5 @@
 		});
 			
 	}
-	if (!window.otzyv_plugin) startPlugin();
-				
+	if (!window.otzyv_plugin) startPlugin();			
 })();
