4d06683ebaeff1183676c3c0299a15d1d710dadf
lampaplugins <152893536+lampaplugins@users.noreply.github.com>
2024-08-25 20:31:06 +0500

    Update o.js

--- o.js
+++ o.js
@@ -28,9 +28,9 @@
 			if (e.type == 'complite') {
 					var num=0;
 					$('.full-start-new__buttons').append('<div class="full-start__button selector button--otzyv"><svg height="34" viewBox="0 0 28 34" fill="none" xmlns="http://www.w3.org/2000/svg"> <rect x="1.5" y="1.5" width="25" height="31" rx="2.5" stroke="currentColor" stroke-width="3"></rect><rect x="6" y="7" width="9" height="9" rx="1" fill="currentColor"></rect><rect x="6" y="19" width="16" height="3" rx="1.5" fill="currentColor"></rect><rect x="6" y="25" width="11" height="3" rx="1.5" fill="currentColor"></rect><rect x="17" y="7" width="5" height="3" rx="1.5" fill="currentColor"></rect> </svg><span>Отзывы</span></div>');
-					if (Lampa.Account.logged()) $.get('https://api.skaz.tv/ts.php?kp='+e.data.movie['kinopoisk_id']+'&imdb='+e.data.movie['imdb_id'], function (data) {
-					if (data!='') $('.full-start-new__details').append('<span class="full-start-new__split">●</span><span>Качество: '+data+'</span>');
-					});
+					//if (Lampa.Account.logged()) $.get('https://api.skaz.tv/ts.php?kp='+e.data.movie['kinopoisk_id']+'&imdb='+e.data.movie['imdb_id'], function (data) {
+					//if (data!='') $('.full-start-new__details').append('<span class="full-start-new__split">●</span><span>Качество: '+data+'</span>');
+					//});
 					$('.button--otzyv').on('hover:enter', function (card) {
 					if (num > 9) num = 0;
 					otzyv_kp_imdb(e.data.movie['kinopoisk_id'],e.data.movie['imdb_id'],num);
