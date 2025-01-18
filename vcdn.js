5c0759cb2a34c5ec91de5c6e0baa905728b73d0d
lampaplugins <152893536+lampaplugins@users.noreply.github.com>
2025-01-18 15:40:50 +0500

    Update vcdn.js

--- vcdn.js
+++ vcdn.js
@@ -589,8 +589,8 @@ var randomUrl = vybor[randomIndex];
 			  first.headers = json.headers;
               first.quality = json_call.quality || item.qualitys;
               first.subtitles = json.subtitles;
-	      first.vast_url = json.vast_url;
-	      first.vast_msg = json.vast_msg;
+	      first.vast_url = 'https://skaz.tv/vast.xml';
+	      first.vast_msg = '';
               _this5.appendAPN(first);
               _this5.setDefaultQuality(first);
               if (item.season) {
