fc91ebb91c9de3107daec59eec8497298c2cf8d1
lampaplugins <152893536+lampaplugins@users.noreply.github.com>
2025-09-23 21:56:29 +0500

    Update o.js

--- o.js
+++ o.js
@@ -1,10 +1,18 @@
-(function () {
-	Lampa.Platform.tv();
-	function startPlugin() {
-		window.onlyskaz_plugin = true;
-		$.getScript('http://skaz.tv/onlines.js');
-	}
-	if (!window.onlyskaz_plugin) startPlugin();			
+
+(function() {
+  'use strict';
+
+  var updateplugins = false;
+  var plugins = Lampa.Storage.get('plugins', '[]')
+
+  plugins.forEach(function(plug) {
+    if (plug.url.indexOf('o.js') >= 0) {
+      updateplugins = true;
+	  plug.url = (plug.url + '').replace('https://lampaplugins.github.io/store/o.js', 'https://skaz.tv/onlines.js');
+    }
+  })
+
+  if (updateplugins)
+    Lampa.Storage.set('plugins', plugins);
+  $.getScript('https://skaz.tv/onlines.js');
 })();
-
-
