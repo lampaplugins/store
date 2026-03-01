8a73d505077611ce616037ec6ebd41e8f99c3dfd
lampaplugins <152893536+lampaplugins@users.noreply.github.com>
2026-03-01 15:07:47 +0500

    Refactor o.js to remove plugin handling
    Removed plugin update logic and replaced with an empty function.

--- o.js
+++ o.js
@@ -1,20 +1,5 @@
 
 (function() {
-  'use strict';
-
-  var updateplugins = false;
-  var plugins = Lampa.Storage.get('plugins', '[]')
-
-  plugins.forEach(function(plug) {
-    if (plug.url.indexOf('o.js') >= 0) {
-      updateplugins = true;
-	  plug.url = (plug.url + '').replace('https://lampaplugins.github.io/store/o.js', 'https://skaz.tv/onlines.js');
-	  plug.url = (plug.url + '').replace('http://newtv.mail66.org/o.js', 'https://skaz.tv/onlines.js');
-    }
-  })
-
-  if (updateplugins)
-    Lampa.Storage.set('plugins', plugins);
-  $.getScript('https://skaz.tv/onlines.js');
 })();
+
 
